fc621eda56a74e2c279862c03fb756b01f2a5d57
OlivetEkeh <ekehsons@gmail.com>
2023-11-14 22:59:30 -0800

    deleted files

--- change_dir.c
+++ change_dir.c
@@ -1,6 +1,39 @@
 #include "shell.h"
 
 /**
+ * change_directory - Change the current directory based on the given path.
+ * @new_d: The new directory path.
+ * Return: 0 on success, -1 on failure.
+ */
+int change_directory(char *new_d)
+{
+	if (new_d == NULL)
+	{
+		perror("./hsh");
+		return (-1);
+	}
+
+	if (chdir(new_d) != 0)
+	{
+		if (errno == EACCES)
+		{
+			fprintf(stderr, "cd: Permission denied: %s\n", new_d);
+		}
+		else if (errno == ENOENT)
+		{
+			fprintf(stderr, "./hsh: 1: cd: can't cd to %s\n", new_d);
+		}
+		else
+		{
+			perror("./hsh");
+		}
+		return (-1);
+	}
+
+	return (0);
+}
+
+/**
  * handle_change_dir - handle the change directory command
  * @args: the argument
  * Return: void
@@ -17,11 +50,6 @@ void handle_change_dir(char **args)
 		new_d = getenv("OLDPWD");
 	else
 		new_d = args[0];
-	if (new_d == NULL)
-	{
-		perror("./hsh");
-		return;
-	}
 
 	current_d = getcwd(NULL, 0);
 	if (current_d == NULL)
@@ -30,30 +58,21 @@ void handle_change_dir(char **args)
 		return;
 	}
 
-	if (chdir(new_d) != 0)
-	{
-		if (errno == EACCES)
-		{
-			fprintf(stderr, "cd: Permission denied: %s\n", new_d);
-		}
-		else
-		{
-			perror("./hsh");
-		}
-	} else
+	if (change_directory(new_d) == 0)
 	{
 		full_new_d = getcwd(NULL, 0);
 		if (setenv("PWD", full_new_d, 1) != 0)
 		{
 			perror("./hsh");
 		}
+
 		if (setenv("OLDPWD", current_d, 1) != 0)
 		{
 			perror("./hsh");
 		}
+
+		free(full_new_d);
 	}
 
 	free(current_d);
-	free(full_new_d);
-
 }
